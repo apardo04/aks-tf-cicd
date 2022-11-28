66838ad00512bf8d9d20207d40c8bcb68051cce6
apardo04 <apardo04@gmail.com>
2022-11-27 22:59:28 -0500

    initial testiing

--- setup-script.sh
+++ setup-script.sh
@@ -7,9 +7,9 @@ storageAccountName="terraformstateadrianpa"
 containerName="terraform"
 
 # Create SPN
-az ad sp create-for-rbac --display-name=$spnName --role="Contributor" --scopes="/subscriptions/7bbf6a19-62e5-4383-bf13-ccc773342b84"
+az ad sp create-for-rbac --display-name=$spnName --role="Owner" --scopes="/subscriptions/7bbf6a19-62e5-4383-bf13-ccc773342b84"
 
-# Create RG, SA, Container
+# Create RG, SA, Container for TF State File
 az group create -l eastus -n $resourceGroupName
 az storage account create -n $storageAccountName -g $resourceGroupName -l eastus --sku Standard_LRS
 az storage container create --name $containerName --account-name $storageAccountName \
